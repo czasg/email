4e756cde8f1c8ce81216b843602fc3cc5c438896
czasg <czasg0.0>
2021-12-04 21:33:35 +0800

    增加json字段

--- email.go
+++ email.go
@@ -8,15 +8,15 @@ import (
 )
 
 type Payload struct {
-	UserAccount string `env:"EMAIL_ACCOUNT"`
-	Secret      string `env:"EMAIL_SECRET"`
-	Subject     string `default:"email by go"`
-	To          []string
-	Body        string `default:"email by go"`
-	Host        string `default:"smtp.qq.com"`
-	Port        string `default:"25"`
-	ContentType string `default:"text/plain; charset=\"utf-8\""`
-	MIMEVersion string `default:"1.0"`
+	UserAccount string   `json:"userAccount" env:"EMAIL_ACCOUNT"`
+	Secret      string   `json:"secret" env:"EMAIL_SECRET"`
+	Subject     string   `json:"subject" default:"email by go"`
+	To          []string `json:"to"`
+	Body        string   `json:"body" default:"email by go"`
+	Host        string   `json:"host" default:"smtp.qq.com"`
+	Port        string   `json:"port" default:"25"`
+	ContentType string   `json:"contentType" default:"text/plain; charset=\"utf-8\""`
+	MIMEVersion string   `json:"mimeVersion" default:"1.0"`
 }
 
 func SendMail(payload Payload) error {
